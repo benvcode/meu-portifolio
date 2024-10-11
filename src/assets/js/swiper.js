a89ba37241ca73114c21c1512253f685a0b8003a
benvcode <benvcode.instructor@gmail.com>
2024-10-11 16:16:51 +0100

    chore: Pequenas mudanças

--- src/assets/js/swiper.js
+++ src/assets/js/swiper.js
@@ -4,3 +4,8 @@ console.log('Executando swiper.js');
 import { register } from 'swiper/element/bundle';
 // register Swiper custom elements
 register();
+
+const swiperContainer = document.querySelector('.swiper');
+swiperContainer.autoplay = {
+  delay: 5000, // 5 segundos de delay
+};
